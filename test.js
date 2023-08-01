b8d7f5c1ddc49766b5d84d3318a038908bae8835
rcmtcristian <rcmtcristian@gmail.com>
2023-07-31 21:48:34 -0400

    💄 Change mobile view
    added a placeholder for mobile view

---
+++ test.js
@@ -0,0 +1,14 @@
+// // Check if the user is using a mobile device
+// function isMobileDevice() {
+//   return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+// }
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   // Get the mobileMessage div
+//   var mobileMessage = document.getElementById('mobileMessage');
+
+//   // Show the div if the user is on a mobile device
+//   if (isMobileDevice()) {
+//     mobileMessage.style.display = 'block';
+//   }
+// });
